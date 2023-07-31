30c7966402000b2c9ad9343dae0e13eebb6955aa
ritik311 <140249721+ritik311@users.noreply.github.com>
2023-07-31 17:47:34 +0530

    First Commit

---
+++ js/index.js
@@ -0,0 +1,60 @@
+// Handle Mouse Clicks
+const numOfDrums = document.querySelectorAll('.drum').length;
+for (var i = 0; i < numOfDrums; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener("click", function () {
+        var key = this.innerHTML;
+        playSound(key);
+        handleButtonFlashing(key)
+    });
+}
+
+// Handle Keyboard Key Presses
+document.addEventListener("keydown", function (event) {
+    var key = event.key;
+    playSound(key);
+});
+
+// Play sound for corresponding key
+function playSound(key) {
+    switch (key) {
+        case 'w':
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        default:
+            console.log("Wrong Key Pressed!");
+    }
+    handleButtonFlashing(key);
+}
+
+// Handle Button Flashing
+function handleButtonFlashing(key){
+    document.querySelector('.'+key).classList.add('pressed')
+    window.setTimeout(function (){
+        document.querySelector('.'+key).classList.remove('pressed');
+    },100);
+}
